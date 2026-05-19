7492906d35d17af20707d1641515f8884ca5d0c3
Derrick Jnr Kello <derrickkello48@gmail.com>
2026-05-19 20:56:19 +0000

    Initial Commit

---
+++ DiskPilot/DiskPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskPilotApp.swift
+//  DiskPilot
+//
+//  Created by MAC on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct DiskPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
